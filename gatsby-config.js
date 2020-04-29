bf6ad02f1620e73ee0cfdf9a013f8beead6cd9ff
Willian Monteiro <willian@zhf.com.br>
2020-04-28 22:06:44 -0300

    Prisma Playground and useStaticQuery

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `PWA REACT GQL`,
+    title: `Willian Monteiro`,
+    position: `Desenvolvedor frontend`,
     description: `Gatsby project`,
     author: `@willianmonteiro`,
   },
@@ -24,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
