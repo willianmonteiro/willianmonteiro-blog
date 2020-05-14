b1bd661b061bdaccd176c29b95cfabb4af89f0f9
willian <willianmntro@gmail.com>
2020-05-14 08:54:48 -0300

    integrate layout base on single post

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import SEO from "../components/seo"
+import Layout from "../components/Layout"
 
 const SinglePost = ({ data }) => {
   const post = data.markdownRemark
   
   
   return (
-    <>
+    <Layout>
+      <SEO title={post.frontmatter.title}/>
       <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    </>
+    </Layout>
   )
 }
 
