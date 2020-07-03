8a2fb52d6d63ca016176fbc330c12e508d0f68d1
willian <willianmntro@gmail.com>
2020-07-02 22:05:59 -0300

    change uploads path

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `uploads`,
-        path: `${__dirname}/public/assets/images`,
+        path: `${__dirname}/static/assets/images`,
       },
     },
     {
