594b4fa304e4b67d10d2d675958c73ba4bf0f8de
willian <willianmntro@gmail.com>
2020-07-10 18:13:49 -0300

    layout mobile margins fixes

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -17,12 +17,18 @@ export const Main = styled.section`
     ${media.lessThan('large')`
       padding: 4.125rem 0 3rem 0;
     `}
+    
+    ${media.lessThan('medium')`
+      padding: 1rem 0 3rem 0;
+    `}
 `
 
 export const LayoutWrapper = styled.section`
-  //display: flex;
-  
   ${media.lessThan('large')`
     flex-direction: column;  
+  `}
+  
+  ${media.lessThan('medium')`
+    margin-bottom: 20px;  
   `}
 `
